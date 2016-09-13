3b7a17c2b80968bfb23e7a035c1ffa2195846962
Daniel Minshew <dm@danbook.local>
2016-09-13 14:01:12 -0500

    add linter

--- tests/homepage.js
+++ tests/homepage.js
@@ -1,13 +1,14 @@
+/* eslint valid-jsdoc: "error" */
 /* eslint-env mocha */
 /* eslint-disable func-names, prefer-arrow-callback */
 // - - Chimp-specific globals:
-/* globals browser assert server */
+/* globals browser assert */
 
 // a regex pattern for semantic version validation
 /* to check, visit regex101.com
 \bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z\-]+(?:\.[\da-z\-]+)*)?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?\
 */
-import semverRegex from 'semver-regex'
+// import semverRegex from 'semver-regex'
 /*              TEST SWEET
 
 |  Acceptance tests for normal UI interactions
@@ -46,23 +47,25 @@ To-do
  *  @description A chimp.js-specific alias for WebDriver's {client} object
  */
 
-function getFooter () {
+function getFooterText() {
     const selectorClass = '.savi-copyright-footer'
-    const cfSelector = browser.elements(selectorClass)
+    const cfSelector = browser
+                          .elements(selectorClass)
+                          .getText()
 
     // this is the synchronous method, we probably
     // really want to return a promise..
-    browser.waitForVisible(selectorClass, 1000)
+    // browser.waitForVisible(selectorClass, 1000)
     // NOTE: can we use Chai-as-Promised yet?
     // ( Mocha uses C-a-P now, as ().should.eventually.have.assertMethod(value) )
 
     return cfSelector
 }
 
-describe('copyright-footer', function() {
-    let copyrightText =
-        `© SAVI Controls, LLC |
-        ##version##`
+describe('copyright-footer', function () {
+    // let copyrightText =
+    //     `© SAVI Controls, LLC |
+    //     ##version##`
 
     beforeEach(() => {
 
@@ -76,13 +79,51 @@ describe('copyright-footer', function() {
     })
 
     it('should exist', function () {
-        console.log(getFooter().selector)
-        assert.ok(getFooter())
+        console.log(getFooterText())
+        // assert.ok(getFooterText())
+
+        const selectorClass = '.savi-copyright-footer'
+        const cfSelector = browser
+            .element(selectorClass)
+            .getText()
+
+        try {
+            console.log(cfSelector)
+        } catch (e) {
+            throw e
+        }
+        assert.ok(cfSelector)
 
     })
 
-    xit('should show a version which is a valid semantic version', function() {
+    // xit('should show a version which is a valid semantic version', function() {
+    //
+    //     assert.ok(semverRegex.test(getFooterText()))
+    // })
+})
 
-        assert.ok(semverRegex.test(getFooter()))
+describe('facility view', function () {
+  // hit the landing page
+  // beforeEach(() => browser.url('http://savi/'))
+    it('should let users select all devices in a group', function () {
+
+        // 1. get # of devices
+        const numberBeforeClick = browser.elements('tv-item', function (e, response) {
+            console.log('e: ' + e)
+            console.log('response: ' + response)
+            console.log('response.value: ' + response.value)
+        })
+        // const numberAfterClick = browser.elements('tv-item selected')
+        // 2. click the 'all' filter
+        // if multiple nodes exist, 'click' will target only the first one
+        browser.timeouts('implicit', 10000)
+        // browser.elementIdClick('preset')
+        console.log(numberBeforeClick)
+        browser.timeoutsImplicitWait(2000)
+        // 3. check that class + selected class match former number
+
+        // assert.equal(numberBeforeClick, numberAfterClick)
+
     })
+
 })
